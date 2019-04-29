f28e3a90cd86f6e4b97f3dc63a81803431a338df
Elan Green <green.elan5@gmail.com>
2019-04-29 14:37:36 -0500

    'cipher'

--- Node/express_practice/app.js
+++ Node/express_practice/app.js
@@ -13,6 +13,22 @@ app.get('/sum', (req, res) => {
   res.send(`The sum of ${a} and ${b} is ${a+b}.`);
 });
 
+app.get('/cipher', (req, res) => {
+  const queries = req.query;
+  const shift = parseInt(queries.shift);
+  const text = queries.text;
+  // abc shift = 1
+  const cipherMessage = text.split('').map(letter => {
+    let charCode = letter.charCodeAt(0);
+    if(charCode === 122) {
+      charCode = 96;
+    }
+    const newCharCode = charCode + shift; 
+    return String.fromCharCode(newCharCode);
+  }).join('');
+  res.send(cipherMessage);
+});
+
 app.listen(8000, () => {
   console.log('Express server is listening on port 8000!');
 });

4b16281105e397bd82b17566f206f28f03a12009
Elan Green <green.elan5@gmail.com>
2019-04-29 14:10:17 -0500

    'sum'

--- Node/express_practice/app.js
+++ Node/express_practice/app.js
@@ -1,3 +1,18 @@
 'use strict';
 
-console.log('start');
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+app.use(morgan('dev'));
+
+app.get('/sum', (req, res) => {
+  const queries = req.query;
+  const a = parseInt(queries.a);
+  const b = parseInt(queries.b);
+  res.send(`The sum of ${a} and ${b} is ${a+b}.`);
+});
+
+app.listen(8000, () => {
+  console.log('Express server is listening on port 8000!');
+});
